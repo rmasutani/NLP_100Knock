5639676f0ec7669566e7384f59c2a85a41a9164b
rmasutani <masutanirugby@gmail.com>
2021-04-18 17:57:42 +0900

    Add some answers

--- ch3/q26.py
+++ ch3/q26.py
@@ -5,10 +5,11 @@ df = pd.read_json('../data/jawiki-country.json', lines=True)
 uk_text = df.query('title=="イギリス"')['text'].values[0]
 
 out = {}
-
+pattern_emp = r"'{3,}(.+?)'{3,}"
 for section in re.findall(r'\|(.+?)\s=\s*(.+)', uk_text):
-    print(section[0], "====", section[1])
-
+    # 強調を取り除く
+    if re.search(pattern_emp, section[1]):
+        print(section[1])
     out[section[0]] = section[1]
 
-print(out)
+# print(out)
